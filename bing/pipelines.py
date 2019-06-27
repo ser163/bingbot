319a2043443eb589ba18c88e0a92541e296c31e7
Harry Liu <l3478830@163.com>
2019-06-28 00:12:01 +0800

    fix 修复背景路径问题

--- bing/pipelines.py
+++ bing/pipelines.py
@@ -29,10 +29,10 @@ class BingPipeline(ImagesPipeline):
         baseroot = settings.IMAGES_STORE
         key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER,
                                     "Control Panel\\Desktop", 0, win32con.KEY_SET_VALUE)
-        win32api.RegSetValueEx(key, "WallpaperStyle", 0, win32con.REG_SZ, "0")
+        win32api.RegSetValueEx(key, "WallpaperStyle", 0, win32con.REG_SZ, "2")
         # 2拉伸适应桌面,0桌面居中
         win32api.RegSetValueEx(key, "TileWallpaper", 0, win32con.REG_SZ, "0")
-        win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER,baseroot + response['path'], 1 + 2)
+        win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER,baseroot +'/' +response['path'], 1 + 2)
         print(response['path'])
 
     def file_path(self, request, response=None, info=None):
